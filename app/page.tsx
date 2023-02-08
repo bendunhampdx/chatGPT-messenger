3ee0e302d7bba216187a9127171b6a13530d828f
Ben Dunham <ben.dunham@theo.agency>
2023-02-07 16:13:45 -0800

    update to use latest nextJS, add first column on homepage

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+function HomePage() {
+	return (
+		<div className='flex flex-col items-center justify-center h-screen px-2 text-white'>
+			<h1 className='text-5xl font-bold mb-20'>ChatGPT</h1>
+
+			<div>
+				<div>
+					<div className='flex flex-col items-center justify-center mb-5'>
+						{/* sun icon */}
+						<h2>Examples</h2>
+					</div>
+					<div className='space-y-2'>
+						<p className='infoText'>"Explain something to me"</p>
+						<p className='infoText'>
+							"What is the difference between a dog and a cat?"
+						</p>
+						<p className='infoText'>"What is the color of the sun?"</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default HomePage;
